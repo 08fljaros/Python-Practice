ccd2c1cac10f86ca185391ca6f67e9ec09dd8d27
aram_se <dk64here@gmail.com>
2022-07-17 14:04:12 -0500

    setting up repo for tracking and progress of python skills over 100 days

---
+++ one_hundred_days.py
@@ -0,0 +1,42 @@
+# 100 Days of Python Code Practice
+
+# - Learning: automation, data science, machine learning, 
+#       game, app, and web development
+# - Tools used: Selenium, Flask, Pandas, NumPy, Scikit Learn, Matplotlib, 
+#       Plotly, Reequest, and Beautiful Soup
+
+# utilizing Udemy course by Dr. Angela Yu
+
+# Day 1: Printing, Commenting, Debugging, String Manipulation and Variables
+# Day 2: Data types, Numbers, Operations, Type Conversions, and f-strings
+# Day 3: Conditional Statements, Logical Operators, Code Blocks, and Scope
+# Day 4:
+# Day 5:
+# Day 6:
+# Day 7:
+# Day 8:
+# Day 9:
+# Day 10:
+
+# Day 11:
+# Day 12:
+# Day 13:
+# Day 14:
+# Day 15:
+# Day 16:
+# Day 17:
+# Day 18:
+# Day 19:
+# Day 20:
+
+# Day 22:
+# Day 22:
+# Day 23:
+# Day 24:
+# Day 25:
+# Day 26:
+# Day 27:
+# Day 28:
+# Day 29:
+# Day 30:
+
